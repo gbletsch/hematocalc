522f25e6a1dace96ea654c7215a8e0219b9d9dc9
Guilherme <gbletsch@gmail.com>
2021-03-07 20:12:34 -0300

    mvp

--- src/components/CVP.js
+++ src/components/CVP.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 function CVP({ bsa }) {
+  const calculateVincristinaDose = () => {
+    let min = 1.2 * Number(bsa);
+    let max = 1.4 * Number(bsa);
+
+    if (min > 2) {
+      return "2";
+    } else if (max > 2) {
+      max = 2;
+    }
+    return `${min.toFixed(2)} - ${max.toFixed(2)}`;
+  };
+
+  const calculateCiclofosfamideDose = () => {
+    return `${(750 * bsa).toFixed(0)} - ${(1000 * bsa).toFixed(0)}`;
+  };
+
   return (
     <div className="cvp__container">
+      <h1 className="cvp__h1">CVP</h1>
       <table className="cvp__table">
         <thead className="cvp__thead">
           <tr className="cvp__tr">
-            <th className="cvp__th">Ordem infusão</th>
+            <th className="cvp__th">Ordem</th>
+            <th className="cvp__th">Medicamento</th>
             <th className="cvp__th">D1</th>
             <th className="cvp__th">D2</th>
             <th className="cvp__th">D3</th>
@@ -17,7 +35,9 @@ function CVP({ bsa }) {
         <tbody className="cvp__tbody">
           <tr className="cvp__tr">
             <td className="cvp__td">1</td>
-            <td className="cvp__td">Vincristina</td>
+            <td className="cvp__td">
+              Vincristina {calculateVincristinaDose()} mg Ev lento
+            </td>
             <td className="cvp__td">X</td>
             <td className="cvp__td"></td>
             <td className="cvp__td"></td>
@@ -26,7 +46,9 @@ function CVP({ bsa }) {
           </tr>
           <tr className="cvp__tr">
             <td className="cvp__td">2</td>
-            <td className="cvp__td">Ciclofosfamida</td>
+            <td className="cvp__td">
+              Ciclofosfamida {calculateCiclofosfamideDose()} mg Ev 1 hora
+            </td>
             <td className="cvp__td">X</td>
             <td className="cvp__td"></td>
             <td className="cvp__td"></td>
@@ -35,12 +57,12 @@ function CVP({ bsa }) {
           </tr>
           <tr className="cvp__tr">
             <td className="cvp__td">3</td>
-            <td className="cvp__td">Prednisona</td>
+            <td className="cvp__td">Prednisona 100mg/dia VO</td>
             <td className="cvp__td">X</td>
-            <td className="cvp__td"></td>
-            <td className="cvp__td"></td>
-            <td className="cvp__td"></td>
-            <td className="cvp__td"></td>
+            <td className="cvp__td">X</td>
+            <td className="cvp__td">X</td>
+            <td className="cvp__td">X</td>
+            <td className="cvp__td">X</td>
           </tr>
         </tbody>
       </table>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,48 +3,70 @@ import {
   calculateBSADubois,
   calculateBSAMosteller,
 } from "../utils/bsa-formulae";
-import BodySurfaceArea from "./BodySurfaceArea";
 import CVP from "./CVP";
 
 function Home() {
+  const DUBOIS = "dubois";
+  const MOSTELLER = "mosteller";
+
   const [height, setHeight] = useState(0);
   const [weight, setWeight] = useState(0);
   const [bsa, setBsa] = useState(0);
-  const [bsaOption, setBsaOption] = useState("dubois");
+  // const [bsaOption, setBsaOption] = useState(DUBOIS);
+
+  const handleBSAOptions = (e) => {
+    const option = e.target.value;
+
+    switch (option) {
+      case DUBOIS:
+        setBsa(calculateBSADubois(height, weight));
+        break;
+      case MOSTELLER:
+        setBsa(calculateBSAMosteller(height, weight));
+        break;
+      default:
+        setBsa(0);
+        break;
+    }
+  };
 
   return (
-    <div className="Home__container">
-      <h2 className="Home__h2">Adicionar dados do paciente:</h2>
-      <form className="Home__basic__form">
-        <label htmlFor="weight" className="Home__label">
-          Peso (kg):
-        </label>
-        <input
-          type="text"
-          className="Home__input"
-          name="weight"
-          id="weight"
-          onChange={(e) => setWeight(e.target.value)}
-        />
-        <label htmlFor="weight" className="Home__label">
-          Altura (cm):
-        </label>
-        <input
-          type="text"
-          className="Home__input"
-          name="height"
-          id="height"
-          onChange={(e) => setHeight(e.target.value)}
-        />
-        <label htmlFor="bsa-options" className="home__label">
-          Fórmula para calcular superfície corporal:
-        </label>
+    <div className="home__container">
+      <h2 className="home__h2">Adicionar dados do paciente:</h2>
+      <form className="home__basic__form">
+        <div className="home__form-item">
+          <label htmlFor="weight" className="home__label">
+            Peso (kg):
+          </label>
+          <input
+            type="text"
+            className="home__input"
+            name="weight"
+            id="weight"
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div className="home__form-item">
+          <label htmlFor="weight" className="home__label">
+            Altura (cm):
+          </label>
+          <input
+            type="text"
+            className="home__input"
+            name="height"
+            id="height"
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
         <select
-          onChange={(e) => setBsaOption(e.target.value)}
+          onChange={handleBSAOptions}
           name="bsa-options"
           id="bsa-options"
           className="home__select"
         >
+          <option value="" selected disabled hidden>
+            Fórmula para calcular superfície corporal{" "}
+          </option>
           <option value="dubois" className="home__option">
             Dubois
           </option>
@@ -53,17 +75,11 @@ function Home() {
           </option>
         </select>
         <p className="home__p">
-          Superfície corporal:{" "}
-          {bsaOption === "dubois"
-            ? calculateBSADubois(height, weight).toFixed(3)
-            : bsaOption === "mosteller"
-            ? calculateBSAMosteller(height, weight).toFixed(3)
-            : null}{" "}
-          m<sup>2</sup>
+          Superfície corporal: {bsa.toFixed(3)} m<sup>2</sup>
         </p>
       </form>
-      <BodySurfaceArea weight={weight} height={height} />
-      <CVP />
+
+      <CVP bsa={bsa} />
     </div>
   );
 }
