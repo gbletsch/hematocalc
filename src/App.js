bcc2a3d0a72d390e960e923f3fe72351288cbdea
Guilherme <gbletsch@gmail.com>
2021-03-12 22:08:12 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import Home from "./components/Home";
 import "./style.css";
 
 function App() {
   return (
     <div className="app__container">
+      <Header />
       <Home />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header__container">
+      <h1>Hemato-calc</h1>
+    </div>
+  );
+}
+
+export default Header;
